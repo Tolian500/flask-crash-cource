642a4649b5b76ef09d57b84f6abc804696b0411b
Tolian500 <tolian500@gmail.com>
2025-03-09 19:41:16 +0100

    test database settings flask

--- app.py
+++ app.py
@@ -1,74 +1,23 @@
-from flask import Flask, request, make_response, render_template, Response, send_from_directory, jsonify, session, flash
+from flask import Flask
 
-import pandas as pd
-import os
-import uuid
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
 
-app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
-app.secret_key = 'changelaterkey'
-
-@app.route('/',methods=['GET','POST'])
-def index():
-    return render_template('index.html', message = 'Index')
+def create_app():
+    app = Flask(__name__, template_folder ='templates')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
 
 
-@app.route('/set_data')
-def set_data():
-    session['name'] = 'Mike'
-    session['other'] = 'Hello world'
-    return render_template('index.html', message = 'Session data set.')
+    db.init_app(app)
 
 
-@app.route('/get_data')
-def get_data():
-    if "name" in session.keys() and "other" in session.keys():
-        name = session['name']
-        other = session['other']
-        return render_template('index.html', message = f'Name: {name}, other: {other}')
-    else:
-        return render_template('index.html', message = 'No session data')
-
-@app.route('/clear_session')
-def clear_session():
-    session.clear()
-    return render_template('index.html', message = 'Session cleared')
+    # import later on
+    from routes import register_routes
+    register_routes(app, db)
 
 
-@app.route('/set_cookie')
-def set_cookie():
-    response = make_response(render_template('index.html', message = 'Cookie set'))
-    response.set_cookie('cookie_name', 'cookie_values')
-    return response
+    migrate = Migrate(app, db)
 
-@app.route('/get_cookie')
-def get_cookie():
-    cookie_value = request.cookies['cookie_name']
-    return render_template('index.html', message = f'Cookie value: {cookie_value}')
-
-
-@app.route('/remove_cookie')
-def remove_cookie():
-    response = make_response(render_template('index.html', message = 'Cookie removed'))
-    response.set_cookie('cookie_name', expires=0)
-    return response
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-    
-        return render_template('login.html')
-    elif request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        if username == 'admin' and password == 'admin':
-            flash('Sucessfull login!')
-            return render_template('index.html', message = '')
-        else: 
-            flash('Login failed!')
-            return render_template('index.html', message = '')
-        
-        
-if __name__ == '__main__':  
-    app.run(host="0.0.0.0", port=5555, debug=True)
+    return app
---
+++ routes.py
@@ -0,0 +1,27 @@
+from flask import render_template, request
+
+from models import Person
+
+def register_routes(app, db):
+    @app.route('/', methods= ['GET', 'POST'])
+    def index():
+
+        if request.method == 'GET':
+            people = Person.query.all()
+
+            return render_template('index.html', people = people)
+        if request.method == 'POST':
+            name=request.form.get('name')
+            age= int(request.form.get('age'))
+            job=request.form.get('job')
+
+            person = Person(name=name,age=age,job=job)
+            db.session.add(person)
+            db.session.commit()
+
+            people = Person.query.all()
+            return render_template('index.html', people = people)
+
+
+
+

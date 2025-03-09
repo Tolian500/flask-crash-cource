96a3f72d33f550dfccf22332f913bb61090202c6
Tolian500 <tolian500@gmail.com>
2025-03-09 22:07:12 +0100

    user authentification and managment

--- app.py
+++ app.py
@@ -1,21 +1,41 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, template_folder ='templates')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
+    app.secret_key = 'secretkey'
 
 
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    from models import User
+
+    @login_manager.user_loader
+    def log_user(uid):
+        return User.query.get(uid)
+
+    @login_manager.user_loader
+    def unauthorised_callback():
+        return redirect(url_for('index'))
+
+    bcrypt = Bcrypt(app)
+
+
 
     # import later on
     from routes import register_routes
-    register_routes(app, db)
+    register_routes(app, db, bcrypt)
 
 
     migrate = Migrate(app, db)
